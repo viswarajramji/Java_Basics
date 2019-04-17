89abf735c168c2a3e2626e0b19506068ef30b26e
Viswarajramji <viswarajramji93@gmail.com>
2019-04-17 23:58:45 +0530

    Sleep operation

--- src/multithreading/sleepMethod.java
+++ src/multithreading/sleepMethod.java
@@ -1,13 +1,22 @@
 package multithreading;
 
-class SleepingMethod implements Runnable{
-
+class SleepingMethod implements Runnable {
 	@Override
 	public void run() {
-     f
-		
+		try {
+			for (int i = 0; i <= 10; i++) {
+				System.out.println(i);
+				Thread.sleep(1000);
+			}
+		} catch (InterruptedException ex) {
+			System.out.println("Thread Interrupted");
+		}
 	}
 }
-public class sleepMethod {
 
+public class sleepMethod {
+	public static void main(String[] args) {
+		Thread t1 = new Thread(new SleepingMethod());
+		t1.start();
+	}
 }
